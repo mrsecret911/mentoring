d158407fd66e6e633a57e8ae5518291a4154fb31
mrsecret911 <stanislavmelnychek@gmail.com>
2016-10-24 00:44:57 +0300

    refactored and added promises

--- public/js/script.js
+++ public/js/script.js
@@ -1,29 +1,130 @@
 $(document).ready( function() {
 var href = window.location.pathname;
+var $nav = $("#nav"),
+$content = $("#content");
 
-function loadPage(href) {
-	console.log(window.location);
+
+function loadData(content) {
+	$("#content").html(content);
+	var pageFunction = $nav.find("li.active a").attr("data-page");
+	pageFunctions[pageFunction]();
+}
+
+
+function loadContent(href) {
+	history.pushState(null, null, href);
 	if(href === '/') {
-		$("#content").load("http://localhost:9999/pages/home/index.html");
+		url = "http://localhost:9999/pages/home.html";
 	}
 	else {
 		history.pushState(null, null, href);
-		$("#content").load("http://localhost:9999" + href +'index.html' );
+		url = "http://localhost:9999" + href;
 	}
+	$.ajax({
+		url: url,
+		type: 'get',
+		dataType: 'html'
+	})
+	.then(loadData)
+	.catch(error => console.error(error))
 }
-loadPage(href);
+
+$nav.on('click', 'a', function (event) {
+	$(this).closest("li").addClass('active').siblings("li").removeClass("active");
+	href = $(this).attr("href");
+	event.preventDefault();
+	loadContent(href);	
+});
 
 
-var $nav = $("#nav"),
- 	$content = $("#content");
+loadContent(href);
+ 	
 
- 	$nav.find("a").click(function (event) {
- 		href = $(this).attr("href");
- 		history.pushState(null, null, href);
- 		loadPage(href);
- 		event.preventDefault();
- 	});
 
+
+var pageFunctions = {
+	home: function(){
+		$(':file').unbind('change').change( function () {
+        var input = $(this),
+                numFiles = input.get(0).files ? input.get(0).files.length : 1,
+                label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+
+        var $txt = $(this).parents('.input-group').find(':text'),
+                log = numFiles > 1 ? numFiles + ' files selected' : label;
+
+        if ($txt.length) {
+          $txt.val(log);
+        }
+
+        readURL(this);
+
+      });
+
+      var allImages = localStorage.getItem("allIamges");
+      if(allImages){
+        var imgArr = JSON.parse(allImages);
+        $("#imgWrap").html('<img src="' + imgArr[imgArr.length - 1].src + '">');
+      }
+
+      function readURL(input) {
+        if (input.files && input.files[0]) {
+          var reader = new FileReader();
+
+          reader.onload = function (e) {
+          	console.log(22);
+
+            var src = e.target.result;
+            $("#imgWrap").html('<img src="' + src + '">');
+
+            if(localStorage.getItem("allIamges")){
+
+              var allImages = JSON.parse(localStorage.getItem("allIamges"));
+              allImages.push({
+                index: allImages.length,
+                src: src
+              });
+              localStorage.setItem('allIamges', JSON.stringify(allImages));
+            }
+            else {
+              var allImages = [{
+                index: 0,
+                src: src
+              }];
+              localStorage.setItem('allIamges', JSON.stringify(allImages));
+            }
+          }
+          reader.readAsDataURL(input.files[0]);
+        }
+
+      }
+
+	},
+	allImages: function(){
+		var allImages = localStorage.getItem("allIamges");
+		var $allImages = $("#allImages");
+
+		if(allImages) {
+			JSON.parse(allImages).forEach(function(el){
+				$allImages.append('<div class="col-xs-3"><img src="' + el.src + '"></div>');
+			});	
+		}
+	},
+	searchResult: function(){
+		$("#search").submit(function(event){
+			$.get( 'https://www.googleapis.com/customsearch/v1?key=AIzaSyBE75WzZrtIJ-M_mKoqC63clmR2SCdskdo&cx=017576662512468239146:omuauf_lfve&q=' + $(this).find("input").val(), function( data ) {
+			  var $searchResults = $("#search_results");
+			  console.log(data.items);
+			    data.items.forEach(function(el) {
+			      var title = el.title;
+			      var link = el.link;
+			      var snippet = el.snippet;
+			      $searchResults.append('<div class="col-xs-12"><a href="' + link + '">' + title + '</a><br/> <p>' + snippet + '</p></div><br/>');
+			    })
+			});
+			event.preventDefault();
+		});
+	},
+}
 });
 
 
--- public/pages/all-images/script.js
+++
@@ -1,8 +0,0 @@
-var allImages = localStorage.getItem("allIamges");
-var $allImages = $("#allImages");
-
-if(allImages) {
-	JSON.parse(allImages).forEach(function(el){
-		$allImages.append('<div class="col-xs-3"><img src="' + el.src + '"></div>');
-	});	
-}
